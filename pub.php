4823d3e61290f43cb4b780e886a7b411395c8148
paperpanks <pnkjgautam05@gmail.com>
2019-03-28 12:41:12 +0530

    fixes

--- pub.php
+++ pub.php
@@ -9,13 +9,129 @@
         $_SESSION['bannerTitle'] = 'Publications';
 		include_once('nav-banner.php'); ?>
 
-
 		<section style="margin-top: 40px;">
-		<div class="container">
-			<div class="row justify-content-center">
-			<?php include_once('publications.php'); ?>
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-12 mt-sm-30 typo-sec">
+						<div class="">
+							<ol class="ordered-list">
+								<li >Johari, Rahul, and Neelima Gupta. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-3-642-22540-6_43&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEnQalVc3MVDIss-n_eNNTlsFe1sw" target="_blank">Insecure query processing in the delay/fault tolerant mobile sensor network (dft-msn) and mobile peer to peer network</a>." In 
+									<em>International Conference on Network Security and Applications</em>, pp. 453-462. Springer, Berlin, Heidelberg, 2011. 
+								</li>
+								<li >Johari, Rahul, and Neelima Gupta. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F6112951&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFwNVEed8JtozMr17i1pEEsSjypfQ" target="_blank">Secure query processing in delay tolerant network using java cryptography architecture</a>." In 
+									<em>Computational Intelligence and Communication Networks (CICN), 2011 International Conference on</em>, pp. 653-657. IEEE, 2011. 
+								</li>
+								<li >Johari, Rahul. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fs3.amazonaws.com%2Facademia.edu.documents%2F37712403%2F2411ijasuc10.pdf%3FAWSAccessKeyId%3DAKIAIWOWYYGZ2Y53UL3A%26Expires%3D1542822405%26Signature%3DIF8e3WW50nGv12VwXDtIuHQsJ44%253D%26response-content-disposition%3Dinline%253B%2520filename%253DANALYSIS_OF_QUERY_BASED_ATTACK_IN_THE_DE.pdf&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFHosf21tjidkMu7VAAI5SjO22QmQ" target="_blank">Analysis of Query based attack in the Delay/Fault Tolerant Mobile Sensor Network</a>." 
+									<em>International Journal of Ad hoc, Sensor &amp; Ubiquitous Computing (IJASUC) Vol</em> 2 (2011). 
+								</li>
+								<li >Juyal, Vandana, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fwww.researchgate.net%2Fprofile%2FVandana_Juyal%2Fpublication%2F265973728_NODE_REACHABILITY_IN_DTN_FOR_INDIAN_SCENARIO%2Flinks%2F55a7702808ae92aac77f8879.pdf&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEdxvPx4AnKX2EX2L-98h1MkzwySw" target="_blank">Node reachability in DTN for Indian Scenario</a>." 
+									<em>Proceedings of IJEST</em> 4, no. 6 (2012): 2560-2566. 
+								</li>
+								<li >Jain, Shikha, and Rahul Johari. "
+									<a href="https://scholar.google.co.in/scholar?hl=en&amp;as_sdt=0,5&amp;cluster=14266739420272109447" target="_blank">SECRA (Secure Erasure Coding based Routing Algorithm)</a>” published in the proceedings of “First International Conference on Research Trends in Computer Technologies (ICRTCT-2013)”, Dr." 
+									<em>NGP Arts and Science College, Coimbatore, Tamil Nadu</em> (2013). 
+								</li>
+								<li >Johari, Rahul, Neelima Gupta, and Sandhya Aneja. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-3-642-37949-9_1&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFSai_ezbL8E8Ak4dHkgL-PNyI4Ng" target="_blank">DSG-PC: dynamic social grouping based routing for non-uniform buffer capacities in DTN supported with periodic carriers</a>." In 
+									<em>International Conference on Heterogeneous Networking for Quality, Reliability, Security and Robustness</em>, pp. 1-15. Springer, Berlin, Heidelberg, 2013. 
+								</li>
+								<li >Bhardwaj, Priti, and Rahul Johari. "
+									<a href="https://scholar.google.com/scholar?cluster=14139772742083437032&amp;hl=en&amp;oi=scholarr" target="_blank">Routing in Delay Tolerant Network using Genetic Algorithm</a>." 
+									<em>IJCET, IAEME</em> 4 (2013): 590-597. 
+								</li>
+								<li >Dahiya, Pragati, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F6779285&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEx_liQ_9LAVUekOW61GsxIMrtx7Q" target="_blank">VAST: Volume adaptive searching technique for optimized routing in mobile ad-hoc networks</a>." In 
+									<em>Advance Computing Conference (IACC), 2014 IEEE International</em>, pp. 1-6. IEEE, 2014. 
+								</li>
+								<li >Mahmood, Dhari Ali, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F6828007&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNElVKMjo13S0AY2J3W1R1nKGbTcjg" target="_blank">Routing in MANET using cluster based approach (RIMCA).</a>" In 
+									<em>Computing for Sustainable Global Development (INDIACom), 2014 International Conference on</em>, pp. 30-36. IEEE, 2014.
+								</li>
+								<li >Johari, Rahul, and Dhari Ali Mahmood. "
+									<a href="https://scholar.google.com/scholar_url?url=https://www.researchgate.net/profile/Rahul_Johari2/publication/274985326_GAACO_Metaheuristic_driven_approach_for_routing_in_OppNet/links/552f73ea0cf2acd38cbbf910/GAACO-Metaheuristic-driven-approach-for-routing-in-OppNet.pdf&amp;hl=en&amp;sa=T&amp;oi=gsb-ggp&amp;ct=res&amp;cd=0&amp;d=3397030340197735085&amp;ei=2xb4W-vIEoX3ygS9h62wCQ&amp;scisig=AAGBfm3yhFW4O9I-m4vqlUdVUyHcjHN_Bg" target="_blank">GAACO: Metaheuristic driven approach for routing in OppNet</a>." In 
+									<em>Computer &amp; Information Technology (GSCIT), 2014 Global Summit on</em>, pp. 1-6. IEEE, 2014. 
+								</li>
+								<li >Bhatia, Abhishek, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F7014728&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNH9CCnCLq4n1H2dES-9sy7BU6IaXg" target="_blank">Genetically optimized ACO inspired PSO algorithm for DTNs</a>." In 
+									<em>Reliability, Infocom Technologies and Optimization (ICRITO)(Trends and Future Directions), 2014 3rd International Conference on</em>, pp. 1-6. IEEE, 2014. 
+								</li>
+								<li >Johari, Rahul, Neelima Gupta, and Sandhya Aneja. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fjournals.sagepub.com%2Fdoi%2Fabs%2F10.1155%2F2015%2F376861&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNE21ky0MF0DLBTOwat02S0g9-zVPQ" target="_blank">POSOP routing algorithm: a DTN routing scheme for information connectivity of health centres in Hilly State of North India</a>." 
+									<em>International Journal of Distributed Sensor Networks</em> 11, no. 6 (2015): 376861. 
+								</li>
+								<li >Bhardwaj, Vibha, Rahul Johari, and Priti Bhardwaj. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F7359278&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNH4RoFzhVwndfCWvI7SDm2pzF8bmQ" target="_blank">Query execution evaluation in wireless network using MyHadoop</a>." In 
+									<em>Reliability, Infocom Technologies and Optimization (ICRITO)(Trends and Future Directions), 2015 4th International Conference on</em>, pp. 1-5. IEEE, 2015.
+								</li>
+								<li >Johari, Rahul, and Sakshi Dhama. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-81-322-2580-5_114&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNG025pY0KuJ7pvvEZ1ompEuteI39A" target="_blank">Routing protocols in delay tolerant networks: application-oriented survey</a>." In 
+									<em>Wireless Communications, Networking and Applications</em>, pp. 1255-1267. Springer, New Delhi, 2016.
+								</li>
+								<li >Johari, Rahul, and Dhari A. Mahmood. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-81-322-2580-5_14&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFJeLkIMOGoouX-Y598h6h_64X1Fg" target="_blank">GA-LORD: Genetic Algorithm and LTPCL-Oriented Routing Protocol in Delay Tolerant Network.</a>" In 
+									<em>Wireless Communications, Networking and Applications</em>, pp. 141-154. Springer, New Delhi, 2016.
+								</li>
+								<li >Johari, Rahul, Neelima Gupta, and Sandhya Aneja. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Farticle%2F10.1186%2Fs13638-015-0357-7&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNGgHEZyVvIw3Htq6Nm7uV3lG-AG0Q" target="_blank">CONCOR: context-aware community-oriented routing for intermittently connected network</a>." 
+									<em>EURASIP Journal on Wireless Communications and Networking</em> 2015, no. 1 (2015): 148.
+								</li>
+								<li >Fatimah, Afreen, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fwww.inderscienceonline.com%2Fdoi%2Fabs%2F10.1504%2FIJAC.2016.082026&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEOfc3R22q2QmYM_rfzrKa7VKtR1A" target="_blank">Delay tolerant network: routing issues and performance</a>." 
+									<em>International Journal of Autonomic Computing</em> 2, no. 2 (2016): 99-113. 
+								</li>
+								<li >Fatimah, Afreen, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fdl.acm.org%2Fcitation.cfm%3Fid%3D2900328&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNHPYs4PGvRT4WNeV0mrCpLXITDBpQ" target="_blank">Part: performance analysis of routing techniques in delay tolerant network</a>." In 
+									<em>Proceedings of the International Conference on Internet of things and Cloud Computing</em>, p. 76. ACM, 2016. 
+								</li>
+								<li >Johari, Rahul, and Dhari Ali Mahmood. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fdl.acm.org%2Fcitation.cfm%3Fid%3D2900329&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEA3ZE6S9ApTgy_TD5DgGd9AUBchg" target="_blank">MeNDARIN: Mobile Education Network Using DTN Approach in North IRAQ.</a>" In 
+									<em>Proceedings of the International Conference on Internet of things and Cloud Computing</em>, p. 77. ACM, 2016. 
+								</li>
+								<li >Johari, Shri Rahul, Neelima Gupta, and Sandhya Aneja. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Farticle%2F10.1007%2Fs11277-015-3109-4&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNHzWv6YF3NXetP4g780Twi-l50K4A" target="_blank">Experimental evaluation of routing schemes for intermittently connected wireless mobile networks</a>." 
+									<em>Wireless Personal Communications</em> 87, no. 3 (2016): 897-921.
+								</li>
+								<li >Kaur, Arvinder, Rahul Johari, and Shikha Jain. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F7748962&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNH90vWciCtanLHTCswslaK2t8V4lQ" target="_blank">NEST-ec: Network security testing of erasure codes</a>." In 
+									<em>Advances in Computing, Communication, &amp; Automation (ICACCA)(Fall), International Conference on</em>, pp. 1-6. IEEE, 2016.
+								</li>
+								<li >Singh, Shilpy, Rahul Johari, and Manpreet Kaur. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F7755151&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEW09Ndv9hlX7KXe9wasP2y5IFyrA" target="_blank">ART: Analysis of routing techniques in Delay Tolerant Network</a>." In 
+									<em>Electrical, Electronics, and Optimization Techniques (ICEEOT), International Conference on</em>, pp. 2537-2542. IEEE, 2016.
+								</li>
+								<li >Fatimah, Afreen, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-981-10-3153-3_73&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFY983BJiaKkv7T6qpfLuXdcnDI7g" target="_blank">BDN: Biodegradable Node, Novel Approach for Routing in Delay Tolerant Network</a>." In 
+									<em>Proceedings of the 5th International Conference on Frontiers in Intelligent Computing: Theory and Applications</em>, pp. 731-739. Springer, Singapore, 2017. 
+								</li>
+								<li >Fatimah, Afreen, Rahul Johari, and Mohd Rekaz Muddassir. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F8229896&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNGE24z-3EfCyCvpDZ29E8-hdF9oZg" target="_blank">DTN routing: Secure message transmission in border defence system</a>." In 
+									<em>Computing, Communication and Automation (ICCCA), 2017 International Conference on</em>, pp. 440-445. IEEE, 2017.
+								</li>
+								<li >Fatimah, Afreen, and Rahul Johari. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Flink.springer.com%2Fchapter%2F10.1007%2F978-981-10-5544-7_40&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFMfMf6mHT4syic-wqv5NTqTrDDqw" target="_blank">Secure Text Dissemination in Delay Tolerant Networks</a>." In 
+									<em>Smart Computing and Informatics</em>, pp. 413-420. Springer, Singapore, 2018.
+								</li>
+								<li >Garg, Prachi, Hemang Kumar, Rahul Johari, Prabhanshu Gupta, and Riya Bhatia. "
+									<a href="https://www.google.com/url?q=https%3A%2F%2Fieeexplore.ieee.org%2Fabstract%2Fdocument%2F8474132&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFQq6530dERnJszsVZGZRlTOphAeA" target="_blank">Enhanced Epidemic Routing Protocol in Delay Tolerant Networks</a>." In 
+									<em>2018 5th International Conference on Signal Processing and Integrated Networks (SPIN)</em>, pp. 396-401. IEEE, 2018.
+								</li>
+								<li >Johari, Rahul, Kalpana Gupta."
+									<a href="http://www.google.com/url?q=http%3A%2F%2Fwww.ijfcc.org%2Findex.php%3Fm%3Dcontent%26c%3Dindex%26a%3Dshow%26catid%3D78%26id%3D874&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNF63J_s7ACyH6vzlFw6GKW3WLNtfA" target="_blank">RED: Resource Management in Remote Inaccessible Terrains by Exploitation of DTN Contacts</a>." 
+									<em>International Journal of Future Computer and Communication </em>vol. 7, no.4, pp. 86-90, 2018.  
+								</li>
+								<li >Johari, Rahul, Prachi Garg, Riya Bhatia, Kalpana Gupta, and Afreen Fatimah. "
+									<a href="https://books.google.co.in/books?hl=en&amp;lr=&amp;id=Im9uDwAAQBAJ&amp;oi=fnd&amp;pg=PT268&amp;dq=Applications+of+DTN&amp;ots=bqpoZ6-sx6&amp;sig=JUGWacWK5lB5nLmqjw7yHEDWong&amp;redir_esc=y#v=onepage&amp;q=Applications%20of%20DTN&amp;f=false" target="_blank">Applications of DTN</a>." 
+									<em>Opportunistic Networks: Mobility Models, Protocols, Security, and Privacy</em> (2018): 134.
+								</li>
+							</ol>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
 		</section>
 
 		<?php include_once('footer.php');

40ff2d5b5549eb17090b1f3f6b558b9d1c5df7ff
Pankaj Kumar Gautam <pnkjgautam05@gmail.com>
2019-04-12 22:35:24 +0530

    Name change

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 				<div class="row fullscreen align-items-center justify-content-between">
 					<div class="col-lg-8 col-md-6 banner-left">
 						<h1 class="banner-t">SWINGER</h1>
-						<h2>RJo's : <span class="colored">S</span>ecurity, <span class="colored">W</span>ireless, <span class="colored">I</span>oT <span class="colored">N</span>etwork <span class="colored">G</span>roup of <span class="colored">E</span>ngineering and <span class="colored">R</span>esearch Lab</h2>
-						<h4 class="banner-sub-t">The Soul of WANGER lab are it's Lab Members,who have worked tirelessly to make it reality.
+						<h2>Rahul Johari's : <span class="colored">S</span>ecurity, <span class="colored">W</span>ireless, <span class="colored">I</span>oT <span class="colored">N</span>etwork <span class="colored">G</span>roup of <span class="colored">E</span>ngineering and <span class="colored">R</span>esearch Lab</h2>
+						<h4 class="banner-sub-t">The Soul of SWINGER lab are it's Lab Members,who have worked tirelessly to make it reality.
 						My heart goes out to them....</h4>
-						<h4 class="banner-sub-t">Primary Focus Area of WANGER Lab is : To Carry out Innovative Research in the Area of Wireless Network,
+						<h4 class="banner-sub-t">Primary Focus Area of SWINGER Lab is : To Carry out Innovative Research in the Area of Wireless Network,
 						Network Sciences and Network Security.</h4>
 					</div>
 					<div class="col-lg-4 col-md-6 banner-img">
@@ -29,7 +29,7 @@
 			<div class="container">
 				<div class="row justify-content-center">
 					<div class="col-md-8 pb-30 header-text">
-						<h1>Wanger-Lab</h1>															
+						<h1>Swinger-Lab</h1>															
 					</div>
 				</div>
 				<div class="row">
@@ -37,7 +37,7 @@
 					<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d56050.78214960749!2d76.9834108291509!3d28.594559902636036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x390d1aafdb891567%3A0x10d270731c930a87!2sGGSIPU%2C+Sector+16+C%2C+Dwarka%2C+Delhi!3m2!1d28.5945644!2d77.0184304!5e0!3m2!1sen!2sin!4v1552744008053"
 						width="100%" height="300" frameborder="0" allowfullscreen></iframe>
 						<a href="#">
-							<h4>THE WANGER LAB AT USI,C&T,<br/>GGSIP UNIVERSITY</h4>
+							<h4>THE SWINGER LAB AT USI,C&T,<br/>GGSIP UNIVERSITY</h4>
 						</a>											
 					</div>
 

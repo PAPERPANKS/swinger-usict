d60601a957b2ad2d4fee80b4ff06866c63e92422
anmolgoel12 <anmolgoel12@gmail.com>
2020-05-03 15:17:06 +0530

    fix

--- internship.php
+++ internship.php
@@ -3,8 +3,8 @@
     <title>Swinger Lab - Internship</title>
     <meta property="og:title" content="Swinger Lab - Internship">
     <meta property="og:description" content="Offering Internships under Swinger Lab.">
-    <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg">
-    <meta property="og:url" content="http://euro-travel-example.com/index.htm">
+    <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/GGSIU_logo.svg/1200px-GGSIU_logo.svg.png">
+    <meta property="og:url" content="https://sites.google.com/view/swingerusict/internship_1">
 </head>
 <body>
     <!-- <script>
@@ -15,7 +15,7 @@
     <?php 
   
 // Redirect browser 
-header("Location: https://sites.google.com/view/swingerusict/internship_1"); 
+// header("Location: https://sites.google.com/view/swingerusict/internship_1"); 
   
 exit; 
 ?> 

f638c05795a545270b03a25e34d7a80af9e1cb81
anmolgoel12 <anmolgoel12@gmail.com>
2020-05-03 15:20:33 +0530

    fix image

--- internship.php
+++ internship.php
@@ -3,7 +3,7 @@
     <title>Swinger Lab - Internship</title>
     <meta property="og:title" content="Swinger Lab - Internship">
     <meta property="og:description" content="Offering Internships under Swinger Lab.">
-    <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/GGSIU_logo.svg/1200px-GGSIU_logo.svg.png">
+    <meta property="og:image" content="https://www.motachashma.com/images/exams/ipu-logo.jpg">
     <meta property="og:url" content="https://sites.google.com/view/swingerusict/internship_1">
 </head>
 <body>

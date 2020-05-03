97aa540ee1ae034001e7b6ec40c00423b7b74f03
Anmol Goel <43706763+anmolgoel12@users.noreply.github.com>
2020-05-03 15:40:18 +0530

    fix

--- internship.php
+++ internship.php
@@ -7,29 +7,11 @@
     <meta property="og:url" content="https://sites.google.com/view/swingerusict/internship_1">
 </head>
 <body>
-<<<<<<< HEAD
+
    <script>
-=======
-    <!-- <script>
->>>>>>> f2bd3ec1c62b79a9caaaa171be7834ea0cc9813e
-        // window.load(()=>{
+
             window.location="https://sites.google.com/view/swingerusict/internship_1";
-        // })
-<<<<<<< HEAD
+       
     </script>
-    <?php 
-  
-// Redirect browser 
-// header("Location: https://sites.google.com/view/swingerusict/internship_1"); 
-=======
-    </script> -->
-    <?php 
-  
-// Redirect browser 
-header("Location: https://sites.google.com/view/swingerusict/internship_1"); 
->>>>>>> f2bd3ec1c62b79a9caaaa171be7834ea0cc9813e
-  
-exit; 
-?> 
 </body>
 </html>

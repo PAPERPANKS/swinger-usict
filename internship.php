23b19a0fbc66b69762fd8d4b195045789689f3d8
anmolgoel12 <anmolgoel12@gmail.com>
2020-05-03 14:59:38 +0530

    added internship redirect

---
+++ internship.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Swinger Lab - Internship</title>
+    <meta property="og:title" content="Swinger Lab - Internship">
+    <meta property="og:description" content="Offering Internships under Swinger Lab.">
+    <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg">
+    <meta property="og:url" content="http://euro-travel-example.com/index.htm">
+</head>
+<body>
+    <script>
+        window.load(()=>{
+            window.href.location="https://sites.google.com/view/swingerusict/internship_1";
+        })
+    </script>
+</body>
+</html>
